48bc07484d128157c34d8887629d0af57c809db5
efpies <efpies@gmail.com>
2012-04-25 06:35:54 +0400

    Рефакторинг Triangle и BezierSurface

--- Triangle.cpp
+++ Triangle.cpp
@@ -3,8 +3,14 @@
 
 #include "Triangle.h"
 #include "Matrix.h"
+
+#include <cstdlib>
+
+#include "Constants.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
+//---------------------------------------------------------------------------
+// Custom methods
 //---------------------------------------------------------------------------
 void Triangle::applyTransform(Matrix *transform)
 {
@@ -23,5 +29,114 @@ void Triangle::applyRotation(const double ax, const double ay)
 void Triangle::draw(TCanvas *)
 {
 
+}
+//---------------------------------------------------------------------------
+void Triangle::draw(TCanvas *canvas, float **zBuffer, TColor frontColor, TColor backColor)
+{
+	TPoint canvasCenter = canvas->ClipRect.CenterPoint();
+
+	double tri[3][3];
+
+	tri[0][0] = a->getX();
+	tri[0][1] = a->getY();
+	tri[0][2] = a->getZ();
+	normalized(canvasCenter, tri[0][0], tri[0][1]);
+
+	tri[1][0] = b->getX();
+	tri[1][1] = b->getY();
+	tri[1][2] = b->getZ();
+	normalized(canvasCenter, tri[1][X], tri[1][Y]);
+
+	tri[2][0] = c->getX();
+	tri[2][1] = c->getY();
+	tri[2][2] = c->getZ();
+	normalized(canvasCenter, tri[2][X], tri[2][Y]);
+
+	double head[3] = {a->getX(),
+					  a->getY(),
+					  a->getZ() };
+
+	normalized(canvasCenter, head[X], head[Y]);
+
+	double second[3] = {b->getX(),
+						b->getY(),
+						b->getZ() };
+
+	normalized(canvasCenter, second[X], second[Y]);
+
+	double third[3] = {c->getX(),
+					   c->getY(),
+					   c->getZ() };
+
+	normalized(canvasCenter, third[X], third[Y]);
+
+	double *vecA;
+	double *vecB;
+
+	vecA = makeVector(head, second);
+	vecB = makeVector(head, third);
+
+	double *cross = crossProduct(vecA, vecB);
+
+	bool isFront = cross[Z] >= head[Z];
+
+	delete [] vecA;
+	delete [] vecB;
+	delete [] cross;
+
+	sort(tri, 1);
+
+	double *p1 = tri[0];
+	double *p2 = tri[1];
+	double *p3 = tri[2];
+	double *p4 = p1;
+
+	for(int y = tri[0][Y]; y <= tri[2][Y]; ++y) {
+		if(y >= 0 && y <= canvas->ClipRect.Height()) {
+			if(y > p2[Y]) {
+				p4 = p3;
+			}
+
+			if(p2[Y] - p4[Y] && p3[Y] - p1[Y]) {
+				double xa;
+				double xb;
+				double za;
+				double zb;
+
+				xa = p4[X] + (p2[X] - p4[X]) * (y - p4[Y]) / (p2[Y] - p4[Y]);
+				xb = p1[X] + (p3[X] - p1[X]) * (y - p1[Y]) / (p3[Y] - p1[Y]);
+				za = p4[Z] + (p2[Z] - p4[Z]) * (y - p4[Y]) / (p2[Y] - p4[Y]);
+				zb = p1[Z] + (p3[Z] - p1[Z]) * (y - p1[Y]) / (p3[Y] - p1[Y]);
+
+				if(xa > xb) {
+					std::swap(xa, xb);
+				}
+
+				for(int x = xa; x <= xb; ++x) {
+					if(canvas->ClipRect.Contains(TPoint(x, y))) {
+						if(xb - xa) {
+							double z = za + (zb - za) * (x - xa) / (xb - xa);
+
+							if(z > zBuffer[y - 1][x - 1]) {
+								zBuffer[y - 1][x - 1] = z;
+								canvas->Pixels[x][y] = isFront ? frontColor : backColor;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
+//---------------------------------------------------------------------------
+void Triangle::sort(double array[3][3], unsigned by)
+{
+	for(int i = 0; i < 2; ++i) {
+		for(int j = 0; j < 2; ++j) {
+			if(array[j][by] > array[j + 1][by]) {
+                swap(array[j], array[j + 1]);
+            }
+        }
+    }
 }
 //---------------------------------------------------------------------------
--- Triangle.h
+++ Triangle.h
@@ -4,11 +4,23 @@
 
 #include "Object3D.h"
 #include "Vertice.h"
+
+#define X 0
+#define Y 1
+#define Z 2
 //---------------------------------------------------------------------------
 class Matrix;
 
 class Triangle : virtual public Object3D
 {
+	private :
+		inline void   normalized(TPoint, double &, double &);
+		inline double *makeVector(double *, double *);
+		inline double *crossProduct(double *, double *);
+		inline void   swap(double *, double *);
+
+		void sort(double [3][3], unsigned);
+
 	public :
 		Triangle() {}
 		Triangle(const Triangle&);
@@ -18,10 +30,14 @@ class Triangle : virtual public Object3D
 		void applyRotation(const double, const double);
 		void draw(TCanvas *);
 
+		void draw(TCanvas *, float **, TColor, TColor);
+
 		Vertice *a;
 		Vertice *b;
 		Vertice *c;
 };
+//---------------------------------------------------------------------------
+// Lifecycle
 //---------------------------------------------------------------------------
 Triangle::Triangle(const Triangle &src)
 {
@@ -35,6 +51,48 @@ Triangle::~Triangle()
 	delete a;
 	delete b;
 	delete c;
+}
+//---------------------------------------------------------------------------
+// Custom methods
+//---------------------------------------------------------------------------
+inline double *Triangle::makeVector(double *a, double *b)
+{
+	double *c = new double[3];
+	c[0] = b[0] - a[0];
+	c[1] = b[1] - a[1];
+	c[2] = b[2] - a[2];
+	return c;
+}
+//---------------------------------------------------------------------------
+inline double *Triangle::crossProduct(double *a, double *b)
+{
+	double *cross = new double[3];
+	cross[X] = a[Y] * b[Z] - a[Z] * b[Y];
+	cross[Y] = -a[X] * b[Z] + a[Z] * b[X];
+	cross[Z] = a[X] * b[Y] - a[Y] * b[X];
+	return cross;
+}
+//---------------------------------------------------------------------------
+inline void Triangle::normalized(TPoint center, double &x, double &y)
+{
+	x = center.x + x * scale;
+	y = center.y - y * scale;
+}
+//---------------------------------------------------------------------------
+inline void Triangle::swap(double *a, double *b)
+{
+	double t[3];
+	t[X] = a[X];
+	t[Y] = a[Y];
+	t[Z] = a[Z];
+
+	a[X] = b[X];
+	a[Y] = b[Y];
+	a[Z] = b[Z];
+
+	b[X] = t[X];
+	b[Y] = t[Y];
+	b[Z] = t[Z];
 }
 //---------------------------------------------------------------------------
 #endif
